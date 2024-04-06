e9c7c637ee29f90a87562a78ee82128562fcdec2
Souravkajla <159424805+Souravkajla@users.noreply.github.com>
2024-04-06 14:19:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// window.onscroll = function () {
+//   scrollFunction();
+// };
+
+function scrollFunction() {
+  var header = document.getElementById("header");
+  if (
+    document.body.scrollTop > 773 ||
+    document.documentElement.scrollTop > 773
+  ) {
+    header.style.backgroundColor = "#ffffff"; // Change background color when scrolled
+  } else {
+    header.style.backgroundColor = "#fdf2e9"; // Revert to default when at top
+  }
+  var headerhead = document.getElementById("headerhead");
+  if (
+    document.body.scrollTop > 900 ||
+    document.documentElement.scrollTop > 900
+  ) {
+    headerhead.style.boxShadow = "0 1.2rem 3.2rem rgba(0, 0, 0, 0.03)"; // Change background color when scrolled
+    headerhead.style.position = "sticky";
+  } else {
+    headerhead.style.boxShadow = "0 0 0 0"; // Revert to default when at top
+    headerhead.style.position = "relative";
+  }
+}
